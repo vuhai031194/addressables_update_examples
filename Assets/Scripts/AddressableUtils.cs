e67677dc809eec2f11258598e7b7c48a4dfd73b1
vuhai031194 <vuhai031194@gmail.com>
2024-01-10 07:38:08 +0700

    Finish init + download + load

--- Assets/Scripts/AddressableUtils.cs
+++ Assets/Scripts/AddressableUtils.cs
@@ -14,18 +14,22 @@ public class AddressableUtils : MonoBehaviour
     [SerializeField] private GameObject _loading;
     [SerializeField] private Text _textContent;
     [SerializeField] private Image _imageContent;
-    
+
     [SerializeField] private Button _buttonInit;
     [SerializeField] private Button _buttonDownload;
     [SerializeField] private Button _buttonClearCache;
+    [SerializeField] private Button _buttonNext;
 
     private float _downloadSizeInMB;
+
+    private int _imageIndex = 1;
 
     private void Awake()
     {
         this._buttonInit.onClick.AddListener(InitializeAsync);
         this._buttonDownload.onClick.AddListener(DownloadAsync);
         this._buttonClearCache.onClick.AddListener(ClearCache);
+        this._buttonNext.onClick.AddListener(Next);
     }
 
 
@@ -33,7 +37,7 @@ public class AddressableUtils : MonoBehaviour
     {
         this._loading.transform.localScale = new Vector3(0, 1, 1);
         this._buttonDownload.enabled = false;
-        this._buttonClearCache.enabled = false;
+        this._buttonClearCache.enabled = true;
         this._textContent.text = "Init First";
     }
 
@@ -47,15 +51,19 @@ public class AddressableUtils : MonoBehaviour
             {
                 _percent = downloadDependencies.GetDownloadStatus().Percent;
 
-                this._textContent.text = $"Downloading {_percent * 100}% ({_downloadSizeInMB * _percent} MB / {_downloadSizeInMB} MB)";
+                this._textContent.text =
+                    $"Downloading {_percent * 100}% ({_downloadSizeInMB * _percent} MB / {_downloadSizeInMB} MB)";
 
                 await UniTask.Yield();
             }
         }
 
-        this._buttonClearCache.enabled = true;
+        this._buttonClearCache.enabled = false;
+
+        this._imageIndex = 1;
+        Next();
     }
-    
+
     private void InitializeAsync()
     {
         this._textContent.text = "Initialize Async";
@@ -69,10 +77,12 @@ public class AddressableUtils : MonoBehaviour
                 {
                     Debug.Log("Addressables.GetDownloadSizeAsync.Completed:@" + Time.time);
                     this._downloadSizeInMB = (float)handle.Result / 1024 / 1024;
-                    Debug.Log("GetDownloadSizeAsync: " + handle.Result + " bytes, which is " + this._downloadSizeInMB + " MB");
-                    this._textContent.text = $"Size of download: {this._downloadSizeInMB.ToString(CultureInfo.InvariantCulture)}";
+                    Debug.Log("GetDownloadSizeAsync: " + handle.Result + " bytes, which is " + this._downloadSizeInMB +
+                              " MB");
+                    this._textContent.text =
+                        $"Size of download: {this._downloadSizeInMB.ToString(CultureInfo.InvariantCulture)}";
                     this._buttonInit.enabled = false;
-                    this._buttonDownload.enabled = true;
+                    this._buttonDownload.enabled = true; 
                 };
             }
             catch (Exception e)
@@ -81,9 +91,44 @@ public class AddressableUtils : MonoBehaviour
             }
         };
     }
-    
-    public void ClearCache()
+
+    private void ClearCache()
     {
+        this._textContent.text = "Clear Cache";
+
+        this._buttonInit.enabled = true;
+
         Addressables.ClearDependencyCacheAsync(PRODUCTS_LABEL);
+    }
+
+    private async UniTask<bool> SetFrame()
+    {
+        try
+        {
+            var sprite = await Addressables.LoadAssetAsync<Sprite>(_imageIndex.ToString());
+            this._imageContent.sprite = sprite;
+            return true;
+        }
+        catch (Exception _)
+        {
+            Debug.Log($"Sprite is null");
+
+            return false;
+        }
+    }
+
+    private async void Next()
+    {
+        var result = await SetFrame();
+
+        if (result)
+        {
+            this._imageIndex++;
+        }
+        else
+        {
+            this._imageIndex = 1;
+            Next();
+        }
     }
 }
